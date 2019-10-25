f97943da44fcd169848e9df5c2090b2220f13819
Andy Green <jamesgreenandy@gmail.com>
2019-10-25 13:51:54 -0500

    added matformfield in public website module

--- pot-detection/src/app/public-website/public-website.module.ts
+++ pot-detection/src/app/public-website/public-website.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule } from '@angular/material/form-field'
 
 @NgModule({
 	declarations: [],
 
-	imports: [ CommonModule, BrowserAnimationsModule ]
+	imports: [ CommonModule, BrowserAnimationsModule, MatFormFieldModule ]
 })
 export class PublicWebsiteModule {}
