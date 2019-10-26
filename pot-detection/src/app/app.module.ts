0be20069fbf22eb715871a8bf093be61d10a8201
Andy Green <jamesgreenandy@gmail.com>
2019-10-25 19:12:49 -0500

    fixed home page routing

--- pot-detection/src/app/app.module.ts
+++ pot-detection/src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { PublicWebsiteModule } from './public-website/public-website.module';
+import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
 	declarations: [ AppComponent ],
-	imports: [ BrowserModule, BrowserAnimationsModule ],
+	imports: [ BrowserModule, BrowserAnimationsModule, RouterModule, PublicWebsiteModule, AppRoutingModule ],
 	providers: [],
 	bootstrap: [ AppComponent ]
 })
--- pot-detection/src/app/public-website/public-website.module.ts
+++ pot-detection/src/app/public-website/public-website.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatFormFieldModule } from '@angular/material/form-field';
+
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
-	declarations: [],
+	declarations: [ HomeComponent ],
 
 	imports: [ CommonModule, BrowserAnimationsModule, MatFormFieldModule ]
 })
